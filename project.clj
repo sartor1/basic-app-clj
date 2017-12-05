6ce1292b784019f7fb7b47bd5b65055a4acdeb94
sartor <sartor1resartus@gmail.com>
2017-12-05 15:48:36 +0300

    Leave only clojure, ring, compojure dependencies

--- project.clj
+++ project.clj
@@ -3,20 +3,16 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.1"]
+                 [ring/ring-core "1.6.3"]
                  [ring/ring-defaults "0.2.1"]
-                 [selmer "1.11.3"]
                  [ring/ring-json "0.3.1"]
-                 [metosin/compojure-api "1.1.11"]
-                 [cheshire "5.8.0"]
+                 [ring/ring-mock "0.3.2"]
+                 [compojure "1.5.1"]]       
 
-                 ]
   :plugins [[lein-ring "0.9.7"]]
   ; We tell the plugin what handler should be monitored.
-  ; In the next example, we will try to make it reloadable?
-  ;why jetty server isn't reloadable and this is??
 
   :ring {:handler lightweight-app.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.2"]]}})
